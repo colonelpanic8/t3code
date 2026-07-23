55436a13986dc5dac16e55207ca53a47c7c0a969
Ivan Malison <ivanmalison@gmail.com>
2026-07-23 15:02:59 -0700

    feat: add user-local project icons

--- apps/server/src/ws.ts
+++ apps/server/src/ws.ts
@@ -1084,6 +1084,7 @@ const makeWsRpcLayer = (
           auth,
           cwd: config.cwd,
           keybindingsConfigPath: config.keybindingsConfigPath,
+          settingsConfigPath: config.settingsPath,
           keybindings: keybindingsConfig.keybindings,
           issues: keybindingsConfig.issues,
           providers,
