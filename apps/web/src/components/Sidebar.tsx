116cf6a6fed7eeacb4ad8d97f627f2362ad25979
Ivan Malison <ivanmalison@gmail.com>
2026-08-10 14:20:21 -0700

    fix(web): import custom snooze dialog primitives

--- apps/web/src/components/Sidebar.tsx
+++ apps/web/src/components/Sidebar.tsx
@@ -163,6 +163,15 @@ import { primaryServerProvidersAtom } from "../state/server";
 import { useThreadRunningTerminalIds } from "../state/terminalSessions";
 import { stackedThreadToast, toastManager } from "./ui/toast";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogPanel,
+  DialogPopup,
+  DialogTitle,
+} from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Menu, MenuPopup, MenuRadioGroup, MenuRadioItem, MenuTrigger } from "./ui/menu";
 import { SidebarContent, SidebarGroup, SidebarMenuButton, useSidebar } from "./ui/sidebar";
