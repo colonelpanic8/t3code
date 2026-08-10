f6c75d6aed3cf7cf408dcff903aabe2e28fac98e
Ivan Malison <IvanMalison@gmail.com>
2026-08-09 22:20:51 -0700

    Rebase PR #4394 onto orchestration v2 + latest main

--- apps/web/src/AppRoot.tsx
+++ apps/web/src/AppRoot.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "@tanstack/react-router";
 import { ElectronBrowserHost } from "./browser/ElectronBrowserHost";
 import { PreviewAutomationHosts } from "./components/preview/PreviewAutomationHosts";
 import { QuitHoldOverlay } from "./components/QuitHoldOverlay";
+import { SelectionNavigationBindings } from "./components/SelectionNavigationBindings";
 import { AppAtomRegistryProvider } from "./rpc/atomRegistry";
 import type { AppRouter } from "./router";
 
@@ -14,6 +15,7 @@ import type { AppRouter } from "./router";
 export function AppRoot({ router }: { readonly router: AppRouter }) {
   return (
     <AppAtomRegistryProvider>
+      <SelectionNavigationBindings />
       <RouterProvider router={router} />
       <PreviewAutomationHosts />
       <ElectronBrowserHost />
